fe4fbb3cfbbfabd17d235ac8038d6e0c9f709522
YuranyUlchur <ulchuryurani80@gmail.com>
2023-04-03 17:02:52 -0500

    script that starts a Flask web application with an routes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """
-script that starts a Flask web application with an routes 
+script that starts a Flask web application with an routes
 """
 from flask import Flask
 
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def text():
     return "C {}".format(text.replace('_', ' '))
 
-if __name__ == '_main_':
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,21 +7,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def text():
     return "C {}".format(text.replace('_', ' '))
+
 
 @app.route('/python/<text>', strict_slashes=False)
 def display_python(text='is_cool'):
     return "Python {}".format(text.replace('_', ' '))
 
-if __name__ == '_main_':
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
