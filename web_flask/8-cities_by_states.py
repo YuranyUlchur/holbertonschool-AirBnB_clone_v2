0603f423b2f7ccf905c994745e65e13fe9216d6b
YuranyUlchur <ulchuryurani80@gmail.com>
2023-04-05 18:51:59 -0500

    add functions and html

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -14,11 +14,12 @@ def teardown_context(self):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
+@app.route('/cities_by_states', strict_slashes=False)
+def states_cities():
     states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
