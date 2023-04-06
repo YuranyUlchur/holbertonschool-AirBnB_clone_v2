0c0333d19bd8185b47b90f9869945da6d637f27d
YuranyUlchur <ulchuryurani80@gmail.com>
2023-04-06 12:19:19 -0500

    add add option where URLs with and without a trailing slash will be treated as equivalent.

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that starts a Flask web application with an route /hbnb
+script that starts a Flask web application with an route
 """
 from flask import Flask, render_template
 from models import storage
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
 """
-script that starts a Flask web application with an route /hbnb
+script that starts a Flask web application with an route
+
 """
-from flask import Flask, render_template
+
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
+"""
+If this option is disabled, Flask will treat URLs
+with and without a trailing slash as equivalent.
+|
+v
+"""
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_context(self):
+def teardown_func(self):
     storage.close()
 
 
 @app.route('/states', strict_slashes=False)
 def states():
-    states = storage.all(State)
-    return render_template('9-states.html', states=states)
+    data = storage.all(State)
+    return render_template('9-states.html', states=data, mode="none")
 
 
 @app.route('/states/<id>', strict_slashes=False)
@@ -26,10 +35,10 @@ def states_id(id):
     data = storage.all(State)
     for state in data.values():
         if state.id == id:
-            return render_template('9-states.html', state=states, mode="id")
+            return render_template('9-states.html', state=state)
     else:
-        return render_template('9-states.html', states=data,  mode="not")
+        return render_template('9-states.html', states=data)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
